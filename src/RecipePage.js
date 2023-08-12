2db05213b1317e205170f24b16db3bd5b38b09df
Evelyn Bart <evelyn.m.bart@gmail.com>
2023-08-12 11:59:09 -0400

    start recipePage ready for creation

---
+++ src/RecipePage.js
@@ -0,0 +1,9 @@
+export function RecipePage() {
+    return (
+        <>
+            <div className="RecipePage"></div>
+            <div className="infoColumn"></div>
+            <div className="recipeSteps"></div>
+        </>
+    );
+}
