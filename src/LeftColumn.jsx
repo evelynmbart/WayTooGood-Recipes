7dfc76fbe898128edcc6ca36513e27f7f39bb5b4
Evelyn Bart <evelyn.m.bart@gmail.com>
2023-08-16 13:29:46 -0400

    update leftColumn styles

--- src/LeftColumn.jsx
+++ src/LeftColumn.jsx
@@ -6,12 +6,12 @@ export function LeftColumn() {
         <div className="leftColumn">
                     <fieldset className="border">
                         <legend><b>WHAT YOU'LL NEED</b></legend>
-                        <ul>
+                        <ul className="list">
                             <li>Peeler</li>
                             <li>Small Pot</li>
                             <li>Baking Sheet</li>
-                            <li>Olive Oil (5 tsp|10 tsp)</li>
-                            <li>Butter (3 tbsp|5 tbsp)</li>
+                            <li>Olive Oil (5 tsp | 10 tsp)</li>
+                            <li>Butter (3 tbsp | 5 tbsp)</li>
                             <li>Large Pan</li>
                             <li>Whisk</li>
                         </ul>
@@ -19,11 +19,11 @@ export function LeftColumn() {
                     <NutritionFacts />
                     <fieldset className="tips">
                         <legend><b>NOTES/TIPS</b></legend>
-                        <ul>
-                            <li>you got this!</li>
-                            <li>don't give up!</li>
+                        <ol>
+                            <li>You got this!</li>
+                            <li>Don't give up!</li>
                             <li>SUPER yum result!</li>
-                        </ul>
+                        </ol>
                     </fieldset>
                 </div>
         </>
